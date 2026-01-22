49c39b55c0454bda81c33d1ec395c061bb4485c3
alexcodes558 <daniel.e.skaer@gmail.com>
2026-01-21 17:48:35 -0800

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "system-ui" }}>
+      <h1>Arduino Virtual Coding Course</h1>
+      <p>Welcome! This is your live homepage.</p>
+    </main>
+  );
+}
